fb127a43557eea1de9edd50e90725edf9c372710
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-10-23 15:07:34 +0200

    Alter playlist to locklist

--- www/js/controllers/controllerLocklistsCtrl.js
+++ www/js/controllers/controllerLocklistsCtrl.js
@@ -1,7 +1,34 @@
 angular.module('controller.LocklistsCtrl', [])
 
-.controller('LocklistsCtrl', ['$scope', 'LocklistsServ', function($scope, LocklistsServ) {
+.controller('LocklistsCtrl', ['$scope', '$ionicModal' ,'LocklistsServ', function($scope, $ionicModal, LocklistsServ) {
   $scope.locklists = LocklistsServ.getLocklist();
+
+      // Create and load the Modal
+    $ionicModal.fromTemplateUrl('templates/new_lock_list.html', function (modal) {
+        $scope.taskModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+
+    // Called when the form is submitted
+    $scope.createLockList = function (lock) {
+        $scope.locklists.push({
+            title: lock.title
+   });
+        $scope.taskModal.hide();
+        lock.title = "";
+    };
+
+    // Open our new task modal
+    $scope.newLockList = function () {
+        $scope.taskModal.show();
+    };
+
+    // Close the new task modal
+    $scope.closeNewLockList = function () {
+        $scope.taskModal.hide();
+    };
 }])
 
 
