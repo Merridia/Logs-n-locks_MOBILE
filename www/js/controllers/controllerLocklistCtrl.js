8c2e9a530dda555267dd067ba738e3c5cb4ae3b9
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-10-29 14:16:40 +0100

    Change in locklist

--- www/js/controllers/controllerLocklistCtrl.js
+++ www/js/controllers/controllerLocklistCtrl.js
@@ -1,9 +1,7 @@
 angular.module('controller.LocklistCtrl', [])
 
 .controller('LocklistCtrl',['$scope', '$stateParams', 'LocklistsServ', function ($scope, $stateParams,LocklistsServ) {
-    $scope.lock = LocklistsServ.getLock($stateParams.locklistId); 
-    $scope.shouldShowDelete = true;
-    $scope.listCanSwipe = true;
+    $scope.lock = LocklistsServ.getLock($stateParams.locklistId);
     
     $scope.open = function () {
         LocklistsServ.toggleLock($stateParams.locklistId);
