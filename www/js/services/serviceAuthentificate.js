c7d23e80c92dd39cfca327cc0040f7a9d6bdbd0c
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-10-30 13:59:58 +0100

    add function send request to server and retrieved back

--- www/js/services/serviceAuthentificate.js
+++ www/js/services/serviceAuthentificate.js
@@ -5,12 +5,36 @@
 //save User in local STorage
 
 //User is Authentificate?
-.service('AuthService', ['$localStorage', function($localStorage){
+.service('AuthService', ['$localStorage', '$http', function($localStorage, $http){
     var User = $localStorage.User;
-    var isLog = false;
+    var isLog = $localStorage.isLog || false;
 
-    this.isLoggedIn = function(){
-        return isLog
+    this.isLoggedIn = function() {
+        return isLog;
+    }
+
+    this.sendUser = function(mail, pwd) {
+    	req = {
+			method: 'POST',
+			url: 'http://localhost:1337/api/login',
+			headers: {
+				'authorization': undefined,
+			},
+			data: { 
+					email: mail, 
+					password: pwd 
+				}
+		}
+		
+		var success = function(result){
+			console.log(result);
+		}
+		
+		var error = function(err){
+			console.log(err);
+		}
+
+    	$http(req).then(success,error);
     }
 
 }]);
