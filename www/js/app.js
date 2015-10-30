bab3c52efd9fa3f13d44b67ab4c6adfdc4ec30c9
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-10-30 09:21:20 +0100

    Change first view

--- www/js/app.js
+++ www/js/app.js
@@ -77,5 +77,5 @@ angular.module('starter', ['ionic', 'Service.Authentificate','ngStorage', 'contr
     });
     
 // if none of the above states are matched, use this as the fallback
-$urlRouterProvider.otherwise('/app/lockslists')
+$urlRouterProvider.otherwise('/app/login')
   });
