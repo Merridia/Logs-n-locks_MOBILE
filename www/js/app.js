b0e41b4b76540d1b3013b53c7aa02259120e1c92
J.droza <jennifer.droza@ynov.com>
2015-11-06 16:26:45 +0100

    add Account and modify some templates

--- www/js/app.js
+++ www/js/app.js
@@ -89,8 +89,20 @@ angular.module('starter', ['ionic', 'Service.Authentificate','ngStorage', 'contr
             },
         },
         authentificate: true
-    });
+    })
     
+    //modifie our profil
+    .state('app.account', {
+        url: '/account',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/account.html',
+                controller: 'UserCtrl',
+            },
+        },
+        authentificate: true
+    });
+
 // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/login')
   });
--- www/js/controllers/controllerUser.js
+++ www/js/controllers/controllerUser.js
@@ -2,9 +2,26 @@
 
 .controller('UserCtrl', ['$scope', '$ionicModal', '$state', '$localStorage', 'AuthService', function ($scope, $ionicModal, $state, $localStorage, AuthService) {
 
+    var User = $localStorage.User;
+    console.log(User)
 	$scope.err = $localStorage.err;
 
 	$scope.login = function(loginData) {
 		AuthService.sendUser(loginData.email, loginData.password);
 	}
+
+    //Afficher les informations de l'utilisateur
+	$scope.User = User
+    console.log(User)
+	$scope.UserFName = User.Firstname;
+	console.log($scope.UserFName);
+	$scope.UserLName = User.Lastname;
+	console.log($scope.UserLName);
+
+	$scope.account = function () {
+	    console.log($scope.User)
+	    $state.go('app.account', {
+	        lockid: $scope.User,
+	    })
+	};
 }]);
